017099e1bf685a2893bf170715fb297d0a94e824
Marcel Kolarcik <60097861+marcelkolarcik@users.noreply.github.com>
2020-02-07 11:28:55 +0000

    code revision

--- js/google-maps.js
+++ js/google-maps.js
@@ -1,28 +1,29 @@
- function initMap(){
-            var map = new google.maps.Map(document.getElementById("map"),{
-                zoom : 6,
-                center: {
-                   lat:  51.903614, lng:  -8.468399  // Cork
-                }
-            });
-
-            var labels = 'ABCDEFGHIJKLMOPQRSXYZW';
-            var locations = [
-                { lat:  51.903614, lng:  -8.468399}, // Cork
-                { lat: 53.350140, lng: -6.266155}, // Dublin
-                { lat:52.668018, lng: -8.630498}, // Limerick
-                 { lat: 53.270962, lng: -9.062691} //Galway
-            ];
-
-            var markers = locations.map(function(location,i){
-                return new google.maps.Marker({
-                    position:location,
-                    label:labels[i % labels.lenght]
-                });
-            });
-
-             // Add a marker clusterer to manage the markers.
-        var markerCluster = new MarkerClusterer(map, markers,
-            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
-    
-        }
+function initMap() {
+	var map = new google.maps.Map( document.getElementById( "map" ), {
+		zoom: 6,
+		center: {
+			lat: 51.903614, lng: -8.468399  // Cork
+		}
+	} );
+	
+	var labels = 'ABCDEFGHIJKLMOPQRSXYZW';
+	var locations = [
+		{ lat: 51.903614, lng: -8.468399 }, // Cork
+		{ lat: 53.350140, lng: -6.266155 }, // Dublin
+		{ lat: 52.668018, lng: -8.630498 }, // Limerick
+		{ lat: 53.270962, lng: -9.062691 } //Galway
+	];
+	
+	var markers = locations.map( function ( location, i ) {
+		return new google.maps.Marker( {
+			                               position: location,
+			                               label   : labels[ i % labels.lenght ]
+		                               } );
+	} );
+	
+	// Add a marker clusterer to manage the markers.
+	var markerCluster = new MarkerClusterer( map, markers,
+	                                         { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' }
+	);
+	
+}
--- js/mailjs.js
+++ js/mailjs.js
@@ -1,16 +1,16 @@
-function sendMail(contactForm) {
-    emailjs.send("gmail", "template_pDNgSwG0", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.email_address.value,
-        "request_of_property": contactForm.request_of_property.value
-    })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        }
-    );
-    return false;  // To block from loading a new page
+function sendMail( contactForm ) {
+	emailjs.send( "gmail", "template_pDNgSwG0", {
+		"from_name"          : contactForm.name.value,
+		"from_email"         : contactForm.email_address.value,
+		"request_of_property": contactForm.request_of_property.value
+	} )
+		.then(
+			function ( response ) {
+				console.log( "SUCCESS", response );
+			},
+			function ( error ) {
+				console.log( "FAILED", error );
+			}
+		);
+	return false;  // To block from loading a new page
 }
